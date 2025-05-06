0c26c4eacd5e29630898388c220e5f513bd3ac95
beka birhanu <beka.b.atomsa@gmail.com>
2025-05-06 16:33:21 +0300

    feat(error): add error

---
+++ error/error.go
@@ -0,0 +1,57 @@
+// Package error provides custom error handling with detailed status codes,
+// public messages, and metadata. The errors are designed to give clear
+// feedback to both the user and the service, helping with precise error
+// classification and debugging.
+//
+// It uses custom application-specific status codes that extend standard
+// HTTP status semantics, providing more granular codes for clearer error
+// handling, especially when integrating with an API or other services.
+package error
+
+import (
+	"fmt"
+
+	"github.com/beka-birhanu/toddler/status"
+)
+
+type ErrorTypes string
+
+type Error struct {
+	StatusCode      status.StatusCode
+	PublicMessage   string
+	ServiceMessage  string
+	PublicMetaData  map[string]string
+	ServiceMetaData map[string]string
+}
+
+// Error implements the error interface.
+// Return a formatted string with status code, messages, and metadata
+func (e *Error) Error() string {
+	return fmt.Sprintf(
+		"{status: %s (%d), publicMessage: '%s', serviceMessage: '%s', publicMetaData: %s, serviceMetaData: %s}",
+		status.GetErrorName(e.StatusCode),
+		e.StatusCode,
+		e.PublicMessage,
+		e.ServiceMessage,
+		formatMetaData(e.PublicMetaData),
+		formatMetaData(e.ServiceMetaData),
+	)
+}
+
+// Helper function to format metadata as a string
+func formatMetaData(metaData map[string]string) string {
+	if len(metaData) == 0 {
+		return "{}"
+	}
+
+	formatted := "{"
+	for key, value := range metaData {
+		formatted += key + ": '" + value + "', "
+	}
+	formatted = formatted[:len(formatted)-2] + "}"
+	return formatted
+}
+
+func (e *Error) NeutralizeOverDetailedStatus() {
+	e.StatusCode = status.SuppressOverDetail(e.StatusCode)
+}
---
+++ error/error_test.go
@@ -0,0 +1,30 @@
+package error_test
+
+import (
+	"testing"
+
+	"github.com/beka-birhanu/toddler/error"
+	"github.com/beka-birhanu/toddler/status"
+)
+
+func TestError_Error(t *testing.T) {
+	err := &error.Error{
+		StatusCode:     status.BadRequestMissingField,
+		PublicMessage:  "Missing required field",
+		ServiceMessage: "Field 'username' is missing in the payload",
+		PublicMetaData: map[string]string{
+			"field": "username",
+		},
+		ServiceMetaData: map[string]string{
+			"requestId": "abc123",
+		},
+	}
+
+	expected := "{status: BadRequest_MissingField (4001), publicMessage: 'Missing required field', serviceMessage: 'Field 'username' is missing in the payload', publicMetaData: {field: 'username'}, serviceMetaData: {requestId: 'abc123'}}"
+
+	actual := err.Error()
+
+	if actual != expected {
+		t.Errorf("unexpected error string.\nExpected:\n%s\nGot:\n%s", expected, actual)
+	}
+}
